38240e74bd108a37869e3f29a6a84e409cb104bf
Aman Mohammed <aman.mohammed@digital.cabinet-office.gov.uk>
2022-10-11 17:42:45 +0100

    OJ-997: added session endpoint integration test

---
+++ integration-tests/src/test/java/gov/uk/di/ipv/cri/common/api/util/IpvCoreStubUtil.java
@@ -0,0 +1,100 @@
+package gov.uk.di.ipv.cri.common.api.util;
+
+import org.apache.http.client.utils.URIBuilder;
+
+import java.io.IOException;
+import java.net.Authenticator;
+import java.net.PasswordAuthentication;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Objects;
+import java.util.Optional;
+
+public class IpvCoreStubUtil {
+
+    private static final String ADDRESS_CRI_DEV = "address-cri-dev";
+
+    public static String getPrivateAPIEndpoint() {
+        return getApiEndpoint(
+                "API_GATEWAY_ID_PRIVATE", "Environment variable PRIVATE API endpoint is not set");
+    }
+
+    private static String getApiEndpoint(String apikey, String message) {
+        String apiEndpoint = System.getenv(apikey);
+        Optional.ofNullable(apiEndpoint).orElseThrow(() -> new IllegalArgumentException(message));
+        return "https://" + apiEndpoint + ".execute-api.eu-west-2.amazonaws.com";
+    }
+
+    public static String getClaimsForUser(String baseUrl, int userDataRowNumber)
+            throws URISyntaxException, IOException, InterruptedException {
+        HttpRequest request =
+                HttpRequest.newBuilder()
+                        .uri(
+                                new URIBuilder(baseUrl)
+                                        .setPath("backend/generateInitialClaimsSet")
+                                        .addParameter("cri", ADDRESS_CRI_DEV)
+                                        .addParameter(
+                                                "rowNumber", String.valueOf(userDataRowNumber))
+                                        .build())
+                        .GET()
+                        .build();
+        return sendHttpRequest(request).body();
+    }
+
+    public static HttpResponse<String> sendHttpRequest(HttpRequest request)
+            throws IOException, InterruptedException {
+
+        String basicAuthUser =
+                Objects.requireNonNull(
+                        System.getenv("IPV_CORE_STUB_BASIC_AUTH_USER"),
+                        "Environment variable IPV_CORE_STUB_BASIC_AUTH_USER is not set");
+        String basicAuthPassword =
+                Objects.requireNonNull(
+                        System.getenv("IPV_CORE_STUB_BASIC_AUTH_PASSWORD"),
+                        "Environment variable IPV_CORE_STUB_BASIC_AUTH_PASSWORD is not set");
+
+        HttpClient client =
+                HttpClient.newBuilder()
+                        .authenticator(
+                                new Authenticator() {
+                                    @Override
+                                    protected PasswordAuthentication getPasswordAuthentication() {
+                                        return new PasswordAuthentication(
+                                                basicAuthUser, basicAuthPassword.toCharArray());
+                                    }
+                                })
+                        .build();
+
+        return client.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+
+    public static String getIPVCoreStubURL() {
+        return Optional.ofNullable(System.getenv("IPV_CORE_STUB_URL"))
+                .orElseThrow(
+                        () ->
+                                new IllegalArgumentException(
+                                        "Environment variable IPV_CORE_STUB_URL is not set"));
+    }
+
+    public static String createRequest(String baseUrl, String jsonString)
+            throws URISyntaxException, IOException, InterruptedException {
+
+        var uri =
+                new URIBuilder(baseUrl)
+                        .setPath("backend/createSessionRequest")
+                        .addParameter("cri", ADDRESS_CRI_DEV)
+                        .build();
+
+        HttpRequest request =
+                HttpRequest.newBuilder()
+                        .uri(uri)
+                        .setHeader("Accept", "application/json")
+                        .setHeader("Content-Type", "application/json")
+                        .POST(HttpRequest.BodyPublishers.ofString(jsonString))
+                        .build();
+
+        return sendHttpRequest(request).body();
+    }
+}
