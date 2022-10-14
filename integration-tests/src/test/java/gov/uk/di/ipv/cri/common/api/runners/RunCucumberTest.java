e5ef53a1bcb6ccf8982fa670f9e063b7fb4443b4
Aman Mohammed <aman.mohammed@digital.cabinet-office.gov.uk>
2022-10-14 13:10:22 +0100

    OJ-997: rebased on main with code tidyup

--- integration-tests/src/test/java/gov/uk/di/ipv/cri/common/api/runners/RunCucumberTest.java
+++ integration-tests/src/test/java/gov/uk/di/ipv/cri/common/api/runners/RunCucumberTest.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {"json:target/cucumber.json", "html:target/default-html-reports"},
-        features = "src/test/resources",
+        features = "src/test/resources/features",
         glue = "gov/uk/di/ipv/cri/common/api/stepDefinitions",
         dryRun = false)
 public class RunCucumberTest {}
