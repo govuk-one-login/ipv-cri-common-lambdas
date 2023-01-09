7bd2b416815e40cb85c21a90ab06b3665b4fb27e
tal-nagra <tal.nagra@digital.cabinet-office.gov.uk>
2023-01-09 11:53:28 +0000

    Update JwtVerifier implementation to receive verification options

--- session2/src/app.ts
+++ session2/src/app.ts
@@ -1,6 +1,4 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
-import { SessionService } from "./services/session-service";
-import { DynamoDbClient } from "./lib/dynamo-db-client";
 import { LambdaInterface } from "@aws-lambda-powertools/commons";
 import { Metrics, MetricUnits } from "@aws-lambda-powertools/metrics";
 import { Logger } from "@aws-lambda-powertools/logger";
@@ -8,12 +6,13 @@ import { SsmClient } from "./lib/param-store-client";
 import { ConfigService } from "./services/config-service";
 import { JwtVerifier } from "./services/jwt-verifier";
 import { JweDecrypter } from "./services/jwe-decrypter";
+import {JWTPayload} from "jose";
 
 const logger = new Logger();
 const metrics = new Metrics();
 const configService = new ConfigService(SsmClient);
 const initPromise = configService.init();
-const AUTHORIZATION_SENT_METRIC = "authorization_sent";
+const SESSION_CREATED_METRIC = "session_created";
 
 class SessionLambda implements LambdaInterface {
     @logger.injectLambdaContext({ clearState: true })
@@ -47,9 +46,11 @@ class SessionLambda implements LambdaInterface {
 
             const decryptedJwt = await new JweDecrypter(configService).decryptJwe(parsedRequestBody.request);
 
-            const payload = await new JwtVerifier(configService).verify(decryptedJwt, parsedRequestBody.client_id);
+            const payload = await this.verifyJwtSignature(decryptedJwt, parsedRequestBody.client_id);
 
             logger.info(`signature verification result: ${JSON.stringify(payload)}`);
+
+            metrics.addMetric(SESSION_CREATED_METRIC, MetricUnits.Count, 1);
 
             return {
                 statusCode: 201,
@@ -57,12 +58,29 @@ class SessionLambda implements LambdaInterface {
             };
         } catch (err: any) {
             logger.error("session lambda error occurred.", err);
-            metrics.addMetric(AUTHORIZATION_SENT_METRIC, MetricUnits.Count, 0);
+            metrics.addMetric(SESSION_CREATED_METRIC, MetricUnits.Count, 0);
             return {
                 statusCode: 500,
                 body: `An error has occurred. ${JSON.stringify(err)}`,
             };
         }
+    }
+    private async verifyJwtSignature(jwt: Buffer, clientId: string): Promise<JWTPayload> {
+        const expectedIssuer = await configService.getJwtIssuer(clientId);
+        const expectedAudience = await configService.getJwtAudience(clientId);
+        return await new JwtVerifier(configService).verify(
+            jwt,
+            clientId,
+            new Set([
+                JwtVerifier.ClaimNames.EXPIRATION_TIME,
+                JwtVerifier.ClaimNames.SUBJECT,
+                JwtVerifier.ClaimNames.NOT_BEFORE,
+            ]),
+            new Map([
+                [JwtVerifier.ClaimNames.AUDIENCE, expectedAudience],
+                [JwtVerifier.ClaimNames.ISSUER, expectedIssuer],
+            ]),
+        );
     }
 }
 
