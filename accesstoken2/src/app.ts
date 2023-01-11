1e9eabf859192f1ca5cb947dc91ed1d3ad8d1a5a
Robin Harrison <robin.harrison@digital.cabinet-office.gov.uk>
2023-01-11 15:13:11 +0000

    Add app.ts

--- accesstoken2/src/app.ts
+++ accesstoken2/src/app.ts
@@ -1,15 +1,34 @@
-import {APIGatewayProxyEvent, APIGatewayProxyResult} from "aws-lambda";
-import {LambdaInterface} from '@aws-lambda-powertools/commons';
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { LambdaInterface } from "@aws-lambda-powertools/commons";
+import { Metrics } from "@aws-lambda-powertools/metrics";
+import { Logger } from "@aws-lambda-powertools/logger";
+import { SessionService } from "./services/session-service";
+import { DynamoDbClient } from "./lib/dynamo-db-client";
+import { SsmClient } from "./lib/param-store-client";
+import { ConfigService } from "./services/config-service";
+import { AccessTokenRequestValidator } from "./services/token-request-validator";
+import { AccessTokenService } from "./services/access-token-service";
 
+const logger = new Logger();
+const metrics = new Metrics();
 
-class AccessTokenLambda implements LambdaInterface {
+const configService = new ConfigService(SsmClient);
+const initPromise = configService.init();
+
+export class AccessTokenLambda implements LambdaInterface {
+    constructor(
+        private accessTokenService: AccessTokenService,
+        private sessionService: SessionService,
+        private accessTokenValidator: AccessTokenRequestValidator,
+    ) {}
+
+    @logger.injectLambdaContext({ clearState: true })
+    @metrics.logMetrics({ throwOnEmptyMetrics: false, captureColdStartMetric: true })
     public async handler(event: APIGatewayProxyEvent, context: any): Promise<APIGatewayProxyResult> {
-        logger.info(`AccessTokenLambda: ${JSON.stringify(event.body)}`);
-        let response: APIGatewayProxyResult;
         try {
             await initPromise;
 
-            //validate the incoming payload
+            // validate the incoming payload
             const requestPayload = event.body;
             if (!requestPayload) {
                 return {
@@ -17,9 +36,8 @@ class AccessTokenLambda implements LambdaInterface {
                     body: `Invalid request missing body`,
                 };
             }
-            const accessTokenRequestValidator = new AccessTokenRequestValidator(configService);
 
-            let validationResult = await accessTokenRequestValidator.validate(requestPayload);
+            let validationResult = await this.accessTokenValidator.validatePayload(requestPayload);
             if (!validationResult.isValid) {
                 return {
                     statusCode: 400,
@@ -28,10 +46,7 @@ class AccessTokenLambda implements LambdaInterface {
             }
 
             const searchParams = new URLSearchParams(requestPayload);
-            const sessionService = new SessionService(DynamoDbClient, configService);
-
             const authCode = searchParams.get("code");
-            logger.info(authCode);
             if (!authCode) {
                 return {
                     statusCode: 400,
@@ -39,7 +54,7 @@ class AccessTokenLambda implements LambdaInterface {
                 };
             }
 
-            const sessionItem = await sessionService.getSessionByAuthorizationCode(authCode);
+            const sessionItem = await this.sessionService.getSessionByAuthorizationCode(authCode);
             if (!sessionItem) {
                 return {
                     statusCode: 400,
@@ -48,56 +63,51 @@ class AccessTokenLambda implements LambdaInterface {
             }
 
             logger.appendKeys({ govuk_signin_journey_id: sessionItem.clientSessionId });
-            logger.info("found session: " + JSON.stringify(sessionItem));
 
-            validationResult = await accessTokenRequestValidator.validateTokenRequest(
+            validationResult = await this.accessTokenValidator.validateTokenRequest(
                 authCode,
                 sessionItem,
                 searchParams.get("client_assertion") as string,
             );
+
             if (!validationResult.isValid) {
-                if(validationResult.errorMsg === "Authorisation code does not match") {
+                // Todo: tidy up error handling
+                if (validationResult.errorMsg === "Authorisation code does not match") {
                     return {
                         statusCode: 403,
                         body: JSON.stringify({
                             message: "Invalid request: Access token expired",
                             code: 1026,
-                        })
-                    }
+                        }),
+                    };
                 } else {
                     return {
                         statusCode: 400,
-                        body: `Invalid request: ${validationResult.errorMsg}`
+                        body: `Invalid request: ${validationResult.errorMsg}`,
                     };
                 }
             }
-            //TODO:  invalid authorization code
-
-            console.log("Success point");
             const bearerAccessTokenTTL = configService.getBearerAccessTokenTtl();
-            console.log(`bearerAccessTokenTTL ${JSON.stringify(bearerAccessTokenTTL)}`);
-            // @ts-ignore
             const accessTokenResponse = await this.accessTokenService.createBearerAccessToken(bearerAccessTokenTTL);
-            console.log(`accessTokenResponse ${JSON.stringify(accessTokenResponse)}`);
-            sessionService.createAccessTokenCode(sessionItem, accessTokenResponse);
+            this.sessionService.createAccessTokenCode(sessionItem, accessTokenResponse);
 
             return {
                 statusCode: 200,
                 body: JSON.stringify(accessTokenResponse),
             };
         } catch (err: any) {
-            logger.error(`access token lambda error occurred ${err}`);
+            logger.error(`Access token lambda error occurred ${err}`);
 
             // TODO: redo error handling
-            if(err.message === "Could not find session Item") {
+            if (err.message === "Could not find session Item") {
                 return {
                     statusCode: 403,
                     body: JSON.stringify({
                         message: "Access token expired",
                         code: 1026,
-                        errorSummary: "1026: Access token expired"
-                    })
-                }
+                        errorSummary: "1026: Access token expired",
+                    }),
+                };
             }
 
             return {
@@ -108,5 +118,7 @@ class AccessTokenLambda implements LambdaInterface {
     }
 }
 
-const handlerClass = new AccessTokenLambda();
+const aService = new AccessTokenRequestValidator(configService);
+const sService = new SessionService(DynamoDbClient, configService);
+const handlerClass = new AccessTokenLambda(new AccessTokenService(), sService, aService);
 export const lambdaHandler = handlerClass.handler.bind(handlerClass);
