8b4f61d14b9b9f54c4836b8104732aa029ab5428
Matthew Bidewell <matthewbidewell@gmail.com>
2023-01-12 15:59:45 +0000

    Add unit tests

---
+++ accesstoken2/tests/unit/app.test.ts
@@ -0,0 +1,57 @@
+jest.mock("../../src/services/config-service", () => {
+    return {
+        ConfigService: jest.fn().mockImplementation(() => {
+            return {
+                init: jest.fn().mockResolvedValue([]),
+                config: {
+                    AddressLookupTableName: "Test",
+                },
+            };
+        }),
+    };
+});
+
+import { DynamoDbClient } from "../../src/lib/dynamo-db-client";
+import { AccessTokenLambda } from "../../src/app";
+import { AccessTokenService } from "../../src/services/access-token-service";
+import { AccessTokenRequestValidator } from "../../src/services/token-request-validator";
+import { SessionService } from "../../src/services/session-service";
+import { APIGatewayProxyEvent } from "aws-lambda/trigger/api-gateway-proxy";
+import { Metrics } from "@aws-lambda-powertools/metrics";
+import { Logger } from "@aws-lambda-powertools/logger";
+
+const mockLogger = jest.mocked(Logger);
+const mockMetrics = jest.mocked(Metrics);
+
+mockLogger.mock =
+
+const mockAccesstokenService = jest.mocked(AccessTokenService) as unknown as AccessTokenService;
+const mockSessionService = jest.mocked(SessionService) as unknown as SessionService;
+const mockAccesstokenRequestValidator = jest.mocked(AccessTokenRequestValidator)  as unknown as AccessTokenRequestValidator;
+const mockDynamoDbClient = jest.mocked(DynamoDbClient);
+
+
+describe("Handler", () => {
+    let accessTokenLambda: AccessTokenLambda;
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+        accessTokenLambda = new AccessTokenLambda(
+            mockAccesstokenService,
+            mockSessionService,
+            mockAccesstokenRequestValidator,
+        );
+    });
+
+    it("should pass", async () => {
+        // const event: APIGatewayProxyEvent = {};
+        try {
+            const test = await accessTokenLambda.handler({} as APIGatewayProxyEvent, null)
+            console.log(test);
+
+        } catch(err) {
+            console.log(err);
+        }
+        expect(true);
+    });
+});
