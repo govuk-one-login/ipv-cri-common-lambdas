7141bb7ded3f9cf75f43f41a76b28a2ade51fcea
Olakunle Jegede <olakunle.jegede@digital.cabinet-office.gov.uk>
2023-01-16 13:21:36 +0000

    separate request to record and signature verification

--- accesstoken2/src/app.ts
+++ accesstoken2/src/app.ts
@@ -21,23 +21,14 @@ export class AccessTokenLambda implements LambdaInterface {
         private accessTokenService: AccessTokenService,
         private sessionService: SessionService,
         private accessTokenValidator: AccessTokenRequestValidator,
-    ) {}
+    ) { }
 
     @logger.injectLambdaContext({ clearState: true })
     @metrics.logMetrics({ throwOnEmptyMetrics: false, captureColdStartMetric: true })
     public async handler(event: APIGatewayProxyEvent, context: any): Promise<APIGatewayProxyResult> {
         try {
             await initPromise;
-
-            // validate the incoming payload
-            const requestPayload = event.body;
-            if (!requestPayload) {
-                return {
-                    statusCode: 400,
-                    body: `Invalid request missing body`,
-                };
-            }
-
+            const requestPayload = event.body as string;
             let validationResult = await this.accessTokenValidator.validatePayload(requestPayload);
             if (!validationResult.isValid) {
                 return {
@@ -48,6 +39,7 @@ export class AccessTokenLambda implements LambdaInterface {
 
             const searchParams = new URLSearchParams(requestPayload);
             const authCode = searchParams.get("code");
+            const client_assertion = searchParams.get("client_assertion") as string
             if (!authCode) {
                 return {
                     statusCode: 400,
@@ -65,11 +57,22 @@ export class AccessTokenLambda implements LambdaInterface {
 
             logger.appendKeys({ govuk_signin_journey_id: sessionItem.clientSessionId });
 
-            validationResult = await this.accessTokenValidator.validateTokenRequest(
+            validationResult = await this.accessTokenValidator.validateTokenRequestToRecord(
                 authCode,
-                sessionItem,
-                searchParams.get("client_assertion") as string,
+                sessionItem
             );
+            const expectedAudience = await configService.getJwtAudience(sessionItem.clientId);
+            if (!expectedAudience) {
+                throw new Error("audience is missing");
+            }
+            const jwtPayload = await this.accessTokenValidator.verifyJwtSignature(
+                Buffer.from(client_assertion, "utf-8"), 
+                sessionItem.clientId,
+                expectedAudience
+            );
+            if (!jwtPayload.jti) {
+                throw new Error("jti is missing");
+            }
 
             if (!validationResult.isValid) {
                 // Todo: tidy up error handling
