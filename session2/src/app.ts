3c1e9ab132227d9af9beaf801831ab2fcd398470
tal-nagra <tal.nagra@digital.cabinet-office.gov.uk>
2023-01-17 11:28:21 +0000

    Add audit service / get default SSM parameters in one call / add tracing

--- session2/src/app.ts
+++ session2/src/app.ts
@@ -1,30 +1,38 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { LambdaInterface } from "@aws-lambda-powertools/commons";
 import { Metrics, MetricUnits } from "@aws-lambda-powertools/metrics";
+import { Tracer } from "@aws-lambda-powertools/tracer";
 import { Logger } from "@aws-lambda-powertools/logger";
-import { SsmClient } from "./lib/param-store-client";
 import { ConfigService } from "./services/config-service";
 import { SessionService } from "./services/session-service";
 import { JwtVerifier } from "./services/jwt-verifier";
 import { JweDecrypter } from "./services/jwe-decrypter";
-import { DynamoDbClient } from "./lib/dynamo-db-client";
 import { PersonIdentityService } from "./services/person-identity-service";
-import { SharedClaims } from "./services/shared-claims";
+import { PersonIdentity } from "./services/person-identity";
 import { SessionRequestValidator } from "./services/session-request-validator";
+import { AuditService } from "./services/audit-service";
+import { DynamoDbClient } from "./lib/dynamo-db-client";
+import { SqsClient } from "./lib/sqs-client";
+import { SsmClient } from "./lib/param-store-client";
+import { AuditEventType } from "./services/audit-event";
+import { SessionRequestSummary } from "./services/session-request-summary";
+import { JWTPayload } from "jose";
 
 const logger = new Logger();
 const metrics = new Metrics();
+const tracer = new Tracer({ captureHTTPsRequests: false });
 const configService = new ConfigService(SsmClient);
-const initPromise = configService.init();
+const configInitPromise = configService.init();
 const SESSION_CREATED_METRIC = "session_created";
 const sessionRequestValidator = new SessionRequestValidator(configService, new JwtVerifier(configService, logger));
 
 class SessionLambda implements LambdaInterface {
+    @tracer.captureLambdaHandler({ captureResponse: false })
     @logger.injectLambdaContext({ clearState: true })
     @metrics.logMetrics({ throwOnEmptyMetrics: false, captureColdStartMetric: true })
     public async handler(event: APIGatewayProxyEvent, context: any): Promise<APIGatewayProxyResult> {
         try {
-            await initPromise;
+            await configInitPromise;
             const deserialisedRequestBody = JSON.parse(event.body!);
             const requestBodyClientId = deserialisedRequestBody.client_id;
             let decryptedJwt = null;
@@ -37,7 +45,6 @@ class SessionLambda implements LambdaInterface {
                     body: "Invalid request: JWE decryption failed",
                 };
             }
-
             const jwtValidationResult = await sessionRequestValidator.validateJwt(decryptedJwt, requestBodyClientId);
             if (!jwtValidationResult.isValid) {
                 return {
@@ -50,26 +57,23 @@ class SessionLambda implements LambdaInterface {
             const sessionService = new SessionService(DynamoDbClient, configService);
             const clientIpAddress = this.getClientIpAddress(event);
             metrics.addDimension("issuer", requestBodyClientId);
-            const sessionId: string = await sessionService.saveSession({
-                clientId: jwtPayload.client_id,
-                redirectUri: jwtPayload.redirect_uri,
-                subject: jwtPayload.sub,
-                persistentSessionId: jwtPayload.persistent_session_id,
-                clientSessionId: jwtPayload.govuk_signin_journey_id,
-                clientIpAddress: clientIpAddress ?? null,
-            });
+
+            const sessionRequestSummary = this.createSessionRequestSummary(jwtPayload, clientIpAddress);
+            const sessionId: string = await sessionService.saveSession(sessionRequestSummary);
 
             logger.appendKeys({ govuk_signin_journey_id: sessionId });
             logger.info("created session");
 
             if (jwtPayload.shared_claims) {
                 await new PersonIdentityService(DynamoDbClient, configService).savePersonIdentity(
-                    jwtPayload.shared_claims as SharedClaims,
+                    jwtPayload.shared_claims as PersonIdentity,
                     sessionId,
                 );
             }
 
             metrics.addMetric(SESSION_CREATED_METRIC, MetricUnits.Count, 1);
+
+            await this.sendAuditEvent(sessionId, sessionRequestSummary, clientIpAddress);
 
             return {
                 statusCode: 201,
@@ -88,6 +92,20 @@ class SessionLambda implements LambdaInterface {
             };
         }
     }
+    private createSessionRequestSummary(
+        jwtPayload: JWTPayload,
+        clientIpAddress: string | undefined,
+    ): SessionRequestSummary {
+        return {
+            clientId: jwtPayload["client_id"] as string,
+            clientIpAddress: clientIpAddress ?? null,
+            clientSessionId: jwtPayload["govuk_signin_journey_id"] as string,
+            persistentSessionId: jwtPayload["persistent_session_id"] as string,
+            redirectUri: jwtPayload["redirect_uri"] as string,
+            state: jwtPayload["state"] as string,
+            subject: jwtPayload.sub as string,
+        };
+    }
     private getClientIpAddress(event: APIGatewayProxyEvent): string | undefined {
         if (event.headers) {
             const ipAddressHeader = "x-forwarded-for";
@@ -100,6 +118,21 @@ class SessionLambda implements LambdaInterface {
             logger.warn(`Unexpected quantity of ${ipAddressHeader} headers encountered: ${ipAddressHeaders.length}`);
         }
         return undefined;
+    }
+    private async sendAuditEvent(
+        sessionId: string,
+        sessionRequest: SessionRequestSummary,
+        clientIpAddress: string | undefined,
+    ) {
+        await new AuditService(configService, SqsClient).sendAuditEvent(AuditEventType.START, {
+            clientIpAddress: clientIpAddress,
+            sessionItem: {
+                sessionId: sessionId,
+                subject: sessionRequest.subject,
+                persistentSessionId: sessionRequest.persistentSessionId,
+                clientSessionId: sessionRequest.clientSessionId,
+            },
+        });
     }
 }
 
--- session2/src/lib/param-store-client.ts
+++ session2/src/lib/param-store-client.ts
@@ -1,3 +1,4 @@
 import { SSMClient } from "@aws-sdk/client-ssm";
+import { fromEnv } from "@aws-sdk/credential-providers";
 
-export const SsmClient = new SSMClient({ region: "eu-west-2" });
+export const SsmClient = new SSMClient({ region: process.env["AWS_REGION"], credentials: fromEnv() });
