a264336f6b31a6c1a701c28e26ccc0156bb1c1d6
Matthew Bidewell <matthewbidewell@gmail.com>
2023-01-18 15:07:02 +0000

    run prettier

--- accesstoken2/src/types/request_payload.ts
+++ accesstoken2/src/types/request_payload.ts
@@ -1,7 +1,7 @@
 export type RequestPayload = {
-    grant_type: string,
-    code: string,
-    redirectUri: string,
-    client_assertion_type: string,
-    client_assertion: string
-}
+    grant_type: string;
+    code: string;
+    redirectUri: string;
+    client_assertion_type: string;
+    client_assertion: string;
+};
--- accesstoken2/tests/unit/services/access-token-service.test.ts
+++ accesstoken2/tests/unit/services/access-token-service.test.ts
@@ -1,21 +1,20 @@
 import { AccessTokenService } from "../../../src/services/access-token-service";
 
 describe("access-token-service", () => {
-  let accessTokenService: AccessTokenService;
+    let accessTokenService: AccessTokenService;
 
-  describe("createBearerAccessToken", () => {
+    describe("createBearerAccessToken", () => {
+        beforeEach(() => {
+            jest.resetAllMocks();
+            accessTokenService = new AccessTokenService();
+        });
 
-    beforeEach(() => {
-      jest.resetAllMocks();
-      accessTokenService = new AccessTokenService();
+        it("should return a bearer token when provided with an expires value", async () => {
+            const output = await accessTokenService.createBearerAccessToken(10);
+            expect(output.token_type).toBe("Bearer");
+            expect(output.expires_in).toBe(10);
+            const token = Buffer.from(output.access_token, "base64url");
+            expect(token.length).toBe(32);
+        });
     });
-
-    it("should return a bearer token when provided with an expires value", async () => {
-      const output = await accessTokenService.createBearerAccessToken(10);
-      expect(output.token_type).toBe("Bearer");
-      expect(output.expires_in ).toBe(10);
-      const token = Buffer.from(output.access_token, "base64url");
-      expect(token.length).toBe(32);
-    });
-  });
 });
