5f83016d3cb7da153ff9141a7ead4ced6ac55b5c
Matthew Bidewell <matthewbidewell@gmail.com>
2023-01-23 14:21:03 +0000

    Common library test

--- accesstoken2/src/app.ts
+++ accesstoken2/src/app.ts
@@ -1,9 +1,11 @@
 import {APIGatewayProxyEvent, APIGatewayProxyResult} from "aws-lambda";
 import {LambdaInterface} from '@aws-lambda-powertools/commons';
+import { test } from "common/app";
 
 
 class AccessTokenLambda implements LambdaInterface {
     public async handler(event: APIGatewayProxyEvent, context: any): Promise<APIGatewayProxyResult> {
+        test();
         console.log("Hello world!");
         return {
             statusCode: 200,
