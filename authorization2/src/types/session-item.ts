4cdb9e7fdd3dc34e37335343b1de1e42c052e79e
Olakunle Jegede <olakunle.jegede@digital.cabinet-office.gov.uk>
2023-01-25 18:28:59 +0000

    OJ-1139: Move authorization handler to lambdas folder

--- authorization2/src/types/session-item.ts
+++
@@ -1,7 +0,0 @@
-export type SessionItem = {
-    sessionId: string;
-    clientId: string;
-    clientSessionId: string;
-    authorizationCode: string;
-    authorizationCodeExpiryDate: number;
-};
