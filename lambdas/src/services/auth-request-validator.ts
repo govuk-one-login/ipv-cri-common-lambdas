73487f3fa621411863471b98af7e5fbc20e3f16b
Sarah Cronk <sarah.cronk@digital.cabinet-office.gov.uk>
2023-02-16 14:36:26 +0000

    application of prettier

--- lambdas/src/services/auth-request-validator.ts
+++ lambdas/src/services/auth-request-validator.ts
@@ -7,7 +7,6 @@ export class AuthorizationRequestValidator {
         sessionClientId: string,
         expectedRedirectUri: string,
     ): ValidationResult {
-        
         if (!queryStringParams) {
             return { isValid: false, errorMsg: "Missing querystring parameters" };
         }
@@ -18,20 +17,16 @@ export class AuthorizationRequestValidator {
         let errorMsg = null;
         if (!clientId) {
             errorMsg = "Missing client_id parameter";
-        }
-        else if (!redirectUri) {
+        } else if (!redirectUri) {
             errorMsg = "Missing redirect_uri parameter";
-        }
-        else if (!responseType) {
+        } else if (!responseType) {
             errorMsg = "Missing response_type parameter";
-        }
-        else if (clientId !== sessionClientId) {
+        } else if (clientId !== sessionClientId) {
             errorMsg = "Invalid client_id parameter";
-        }
-        else if (redirectUri !== expectedRedirectUri) {
+        } else if (redirectUri !== expectedRedirectUri) {
             errorMsg = "Invalid redirect_uri parameter";
         }
-        
+
         return { isValid: !errorMsg, errorMsg: errorMsg };
     }
 }
