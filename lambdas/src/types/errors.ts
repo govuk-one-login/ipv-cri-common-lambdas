fc359e51a7ac31591b39067c89102f0a9b0a9b02
Suraj Kumar <suraj.kumar@digital.cabinet-office.gov.uk>
2023-03-01 20:48:12 +0000

    OJ-1265: Updated session-item to include expiryDate

--- lambdas/src/types/errors.ts
+++ lambdas/src/types/errors.ts
@@ -134,3 +134,11 @@ export class AuthorizationCodeExpiredError extends BaseError {
         this.code = 1027;
     }
 }
+
+export class SessionExpiredError extends BaseError {
+    constructor() {
+        super("Session Expired");
+        this.statusCode = 403;
+        this.code = 1030;
+    }
+}
