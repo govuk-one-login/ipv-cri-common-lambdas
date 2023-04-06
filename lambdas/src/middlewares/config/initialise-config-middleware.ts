b9095efde9a207237a5976382ffb8a76cd8d77f0
Olakunle Jegede <olakunle.jegede@digital.cabinet-office.gov.uk>
2023-04-06 17:40:24 +0100

    OJ-1413: Make initialise config support options

--- lambdas/src/middlewares/config/initialise-config-middleware.ts
+++ lambdas/src/middlewares/config/initialise-config-middleware.ts
@@ -4,12 +4,15 @@ import { AwsClientType, createClient } from "../../common/aws-client-factory";
 import { ConfigService } from "../../common/config/config-service";
 import { CommonConfigKey } from "../../types/config-keys";
 
+const defaults = {};
+
 const ssmClient = createClient(AwsClientType.SSM) as SSMClient;
 const configService = new ConfigService(ssmClient);
-const initPromise = configService.init([CommonConfigKey.SESSION_TABLE_NAME, CommonConfigKey.SESSION_TTL]);
-const initialiseConfigMiddleware = (): MiddlewareObj => {
+const initialiseConfigMiddleware = (opts: { config_keys: Array<CommonConfigKey>}): MiddlewareObj => {
+    const options = { ...defaults, ...opts };
+
     const before = async (request: Request) => {
-        await initPromise;
+        await configService.init(options.config_keys);
 
         await request.event;
     };
