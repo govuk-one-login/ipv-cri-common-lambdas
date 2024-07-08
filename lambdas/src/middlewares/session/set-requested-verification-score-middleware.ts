46be24bfab8686963dce4c5c1615f048d2c2c8b3
Mike Beeby <mike.beeby@digital.cabinet-office.gov.uk>
2024-07-08 10:02:52 +0100

    Rename to requested_verification_score

--- lambdas/src/middlewares/session/set-requested-verification-score-middleware.ts
+++ lambdas/src/middlewares/session/set-requested-verification-score-middleware.ts
@@ -5,7 +5,7 @@ const setRequestedVerificationScoreMiddleware = (logger: Logger): MiddlewareObj 
     const before = async (request: Request) => {
         const { evidence_requested } = request.event.body;
         if (evidence_requested?.verificationScore) {
-            logger.appendKeys({ verification_score: evidence_requested.verificationScore });
+            logger.appendKeys({ requested_verification_score: evidence_requested.verificationScore });
             await request.event;
         }
     };
