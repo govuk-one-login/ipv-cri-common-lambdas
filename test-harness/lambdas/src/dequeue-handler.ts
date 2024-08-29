310e284a2ade83bbb665722a408e2941a3eac7a0
Caitlin Cooling <caitlin.cooling@digital.cabinet-office.gov.uk>
2024-08-29 12:25:28 +0100

    OJ-2735: Add linter

--- test-harness/lambdas/src/dequeue-handler.ts
+++ test-harness/lambdas/src/dequeue-handler.ts
@@ -8,22 +8,21 @@ const logger = new Logger();
 // const metrics = new Metrics();
 // const tracer = new Tracer({ captureHTTPsRequests: false });
 
-
 export class DequeueLambdaHandler implements LambdaInterface {
-  async handler(event: SQSEvent): Promise<any> {
-		logger.info("Starting to process records");
-		// const batchFailures: BatchItemFailure[] = [];
+    async handler(event: SQSEvent): Promise<any> {
+        logger.info("Starting to process records");
+        // const batchFailures: BatchItemFailure[] = [];
 
-		for await (const record of event.Records) {
-			try {
-        // push to DDB
-			} catch (error) {
-				// batchFailures.push(new BatchItemFailure(record.messageId));
-				logger.error({ message: "Error writing events to DB", error })
-			}
-		}
+        for await (const record of event.Records) {
+            try {
+                // push to DDB
+            } catch (error) {
+                // batchFailures.push(new BatchItemFailure(record.messageId));
+                logger.error({ message: "Error writing events to DB", error });
+            }
+        }
 
-		logger.info("Finished processing records");
-		// return { batchItemFailures: batchFailures };
-  }
+        logger.info("Finished processing records");
+        // return { batchItemFailures: batchFailures };
+    }
 }
