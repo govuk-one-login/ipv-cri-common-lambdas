abca9a756a521767bbcaa9e80b138c11f4e97c50
Caitlin Cooling <caitlin.cooling@digital.cabinet-office.gov.uk>
2024-08-29 17:00:05 +0100

    OJ-2735: Events are being saved to DB table

--- test-harness/lambdas/src/dequeue-handler.ts
+++ test-harness/lambdas/src/dequeue-handler.ts
@@ -1,12 +1,11 @@
+import { PutItemCommand, DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import type { LambdaInterface } from "@aws-lambda-powertools/commons/types";
 import { Logger } from "@aws-lambda-powertools/logger";
-// import { Metrics } from "@aws-lambda-powertools/metrics";
-// import { Tracer } from "@aws-lambda-powertools/tracer";
 import { SQSEvent } from "aws-lambda";
 
 const logger = new Logger();
-// const metrics = new Metrics();
-// const tracer = new Tracer({ captureHTTPsRequests: false });
+
+const dbClient = new DynamoDBClient({ region: process.env.REGION });
 
 export class DequeueLambdaHandler implements LambdaInterface {
     async handler(event: SQSEvent): Promise<any> {
@@ -14,9 +13,23 @@ export class DequeueLambdaHandler implements LambdaInterface {
         // const batchFailures: BatchItemFailure[] = [];
 
         for await (const record of event.Records) {
+            const nowInSeconds = Math.floor(Date.now() / 1000)
+            const ttl = nowInSeconds + 360
+            const eventData = JSON.parse(record.body);
+
+            const putItemCommand: PutItemCommand = new PutItemCommand({
+                TableName: "audit-events-test-harness-caitlin",
+                Item: {
+                    partitionKey: { S: `SESSION#${eventData.session_id}` },
+                    sortKey: { S: `TXMA#${eventData.event_name}#${eventData.timestamp}#${record.messageId}` },
+                    event: { S: record.body },
+                    expiryDate: { N: ttl.toString() },
+                },
+            })
+
             try {
-                console.log("record", record);
-                // push to DDB
+                await dbClient.send(putItemCommand)
+                logger.info({ message: "Event successfully saved to events table", sessionId: eventData.session_id })
             } catch (error) {
                 // batchFailures.push(new BatchItemFailure(record.messageId));
                 logger.error({ message: "Error writing events to DB", error });
