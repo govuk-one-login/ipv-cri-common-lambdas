bf47648e2d694e4f4e34b70f824db6fdf35af1b0
Caitlin Cooling <caitlin.cooling@digital.cabinet-office.gov.uk>
2024-08-29 17:09:50 +0100

    OJ-2735: Refactoring

--- test-harness/lambdas/src/dequeue-handler.ts
+++ test-harness/lambdas/src/dequeue-handler.ts
@@ -1,43 +1,53 @@
 import { PutItemCommand, DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import type { LambdaInterface } from "@aws-lambda-powertools/commons/types";
 import { Logger } from "@aws-lambda-powertools/logger";
-import { SQSEvent } from "aws-lambda";
+import { SQSEvent, SQSBatchItemFailure } from "aws-lambda";
 
 const logger = new Logger();
-
 const dbClient = new DynamoDBClient({ region: process.env.REGION });
 
 export class DequeueLambdaHandler implements LambdaInterface {
-    async handler(event: SQSEvent): Promise<any> {
+    async handler(event: SQSEvent): Promise<{ batchItemFailures: SQSBatchItemFailure[] }> {
         logger.info("Starting to process records");
-        // const batchFailures: BatchItemFailure[] = [];
 
-        for await (const record of event.Records) {
-            const nowInSeconds = Math.floor(Date.now() / 1000)
-            const ttl = nowInSeconds + 360
-            const eventData = JSON.parse(record.body);
+        const tableName = process.env.EVENTS_TABLE_NAME;
+        const batchItemFailures: SQSBatchItemFailure[] = [];
+
+        for await (const { body, messageId } of event.Records) {
+            const { sessionId, eventName, timestamp } = this.getEventData(body);
+            const nowInSeconds = Math.floor(Date.now() / 1000);
+            const ttl = nowInSeconds + 360;
 
             const putItemCommand: PutItemCommand = new PutItemCommand({
-                TableName: "audit-events-test-harness-caitlin",
+                TableName: tableName,
                 Item: {
-                    partitionKey: { S: `SESSION#${eventData.session_id}` },
-                    sortKey: { S: `TXMA#${eventData.event_name}#${eventData.timestamp}#${record.messageId}` },
-                    event: { S: record.body },
+                    partitionKey: { S: `SESSION#${sessionId}` },
+                    sortKey: { S: `TXMA#${eventName}#${timestamp}#${messageId}` },
+                    event: { S: body },
                     expiryDate: { N: ttl.toString() },
                 },
-            })
+            });
 
             try {
-                await dbClient.send(putItemCommand)
-                logger.info({ message: "Event successfully saved to events table", sessionId: eventData.session_id })
+                await dbClient.send(putItemCommand);
+                logger.info(`Event successfully saved to ${tableName} table`);
             } catch (error) {
-                // batchFailures.push(new BatchItemFailure(record.messageId));
+                batchItemFailures.push({ itemIdentifier: messageId });
                 logger.error({ message: "Error writing events to DB", error });
             }
         }
 
         logger.info("Finished processing records");
-        // return { batchItemFailures: batchFailures };
+        return { batchItemFailures };
+    }
+
+    getEventData(eventBody: string) {
+        const body = JSON.parse(eventBody);
+        return {
+            eventName: body.event_name,
+            sessionId: body.user.session_id,
+            timestamp: body.timestamp,
+        };
     }
 }
 
