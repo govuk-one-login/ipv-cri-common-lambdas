bc0fd96c41048c5a8228d417584a37438461eec1
Caitlin Cooling <caitlin.cooling@digital.cabinet-office.gov.uk>
2024-08-29 11:59:24 +0100

    OJ-2735: Add skeleton template and lambda

---
+++ test-harness/lambdas/src/dequeue-handler.ts
@@ -0,0 +1,29 @@
+import type { LambdaInterface } from "@aws-lambda-powertools/commons/types";
+import { Logger } from "@aws-lambda-powertools/logger";
+// import { Metrics } from "@aws-lambda-powertools/metrics";
+// import { Tracer } from "@aws-lambda-powertools/tracer";
+import { SQSEvent } from "aws-lambda";
+
+const logger = new Logger();
+// const metrics = new Metrics();
+// const tracer = new Tracer({ captureHTTPsRequests: false });
+
+
+export class DequeueLambdaHandler implements LambdaInterface {
+  async handler(event: SQSEvent): Promise<any> {
+		logger.info("Starting to process records");
+		// const batchFailures: BatchItemFailure[] = [];
+
+		for await (const record of event.Records) {
+			try {
+        // push to DDB
+			} catch (error) {
+				// batchFailures.push(new BatchItemFailure(record.messageId));
+				logger.error({ message: "Error writing events to DB", error })
+			}
+		}
+
+		logger.info("Finished processing records");
+		// return { batchItemFailures: batchFailures };
+  }
+}
