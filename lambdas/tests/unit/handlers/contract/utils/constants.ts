2e73d52181e6d1a091f98eb7a08f2fe4f3287c59
Olakunle Jegede <olakunle.jegede@digital.cabinet-office.gov.uk>
2024-09-13 09:41:40 +0100

    OJ-2651: Make token endpoint more generic
    The express application that exposes the /token endpoint needs
    to be more generic as it would be used by all CRI's that consumes
    the typescript token endpoint
    
    Co-authored-by: Mike Beeby <mike.beeby@digital.cabinet-office.gov.uk>

--- lambdas/tests/unit/handlers/contract/utils/constants.ts
+++ lambdas/tests/unit/handlers/contract/utils/constants.ts
@@ -2,11 +2,8 @@ export enum LogLevel {
     DEBUG = "DEBUG",
     INFO = "INFO",
 }
-export enum Provider {
-    EXPERIAN_KBV_CRI_TOKEN_PROVIDER = "ExperianKbvCriTokenProvider",
-}
 export class Constants {
-    static readonly TOKEN = "/token";
+    static readonly TOKEN_ENDPOINT = "/token";
     static readonly LOCAL_APP_PORT = 3000;
     static readonly HTTP_CONTENT_TYPE_HEADER = "Content-Type";
     static readonly JSON_CONTENT_TYPE = "application/json";
