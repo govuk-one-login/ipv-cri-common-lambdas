824f78fc228a0b4fb610ed402b7782314aa1c741
SarahHillGDS <sarah.hill@digital.cabinet-office.gov.uk>
2025-03-20 15:46:59 +0000

    LIME-1592 WIP

--- session/src/main/java/uk/gov/di/ipv/cri/common/api/service/KMSRSADecrypter.java
+++ session/src/main/java/uk/gov/di/ipv/cri/common/api/service/KMSRSADecrypter.java
@@ -9,11 +9,14 @@ import com.nimbusds.jose.crypto.impl.AlgorithmSupportMessage;
 import com.nimbusds.jose.crypto.impl.ContentCryptoProvider;
 import com.nimbusds.jose.jca.JWEJCAContext;
 import com.nimbusds.jose.util.Base64URL;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import software.amazon.awssdk.core.SdkBytes;
 import software.amazon.awssdk.services.kms.KmsClient;
 import software.amazon.awssdk.services.kms.model.DecryptRequest;
 import software.amazon.awssdk.services.kms.model.DecryptResponse;
 import software.amazon.awssdk.services.kms.model.EncryptionAlgorithmSpec;
+import software.amazon.awssdk.services.kms.model.IncorrectKeyException;
 
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
@@ -21,11 +24,26 @@ import javax.crypto.spec.SecretKeySpec;
 import java.util.Objects;
 import java.util.Set;
 
+import static software.amazon.awssdk.services.kms.model.EncryptionAlgorithmSpec.RSAES_OAEP_SHA_256;
+
 class KMSRSADecrypter implements JWEDecrypter {
     private static final Set<JWEAlgorithm> SUPPORTED_ALGORITHMS = Set.of(JWEAlgorithm.RSA_OAEP_256);
     private static final Set<EncryptionMethod> SUPPORTED_ENCRYPTION_METHODS =
             Set.of(EncryptionMethod.A256GCM);
+    private static final Logger LOGGER = LogManager.getLogger();
 
+    // TODO: Set as Env variables
+    //    private static final String CLIENT_JAR_KMS_ENCRYPTION_KEY_ALIAS_PRIMARY = System.getenv(
+    // "CLIENT_JAR_KMS_ENCRYPTION_KEY_ALIAS_PRIMARY");
+    //    private static final String CLIENT_JAR_KMS_ENCRYPTION_KEY_ALIAS_SECONDARY = System.getenv(
+    // "CLIENT_JAR_KMS_ENCRYPTION_KEY_ALIAS_SECONDARY");
+    private static final String CLIENT_JAR_KMS_ENCRYPTION_KEY_ALIAS_PRIMARY =
+            "SigningKeyActiveAlias";
+    private static final String CLIENT_JAR_KMS_ENCRYPTION_KEY_ALIAS_SECONDARY =
+            "SigningKeyPreviousAlias";
+
+    private final boolean keyRotationEnabled =
+            Boolean.parseBoolean(System.getenv("ENV_VAR_FEATURE_FLAG_KEY_ROTATION"));
     private final JWEJCAContext jcaContext;
     private final KmsClient kmsClient;
     private final String keyId;
@@ -64,16 +82,53 @@ class KMSRSADecrypter implements JWEDecrypter {
             throw new JOSEException(
                     AlgorithmSupportMessage.unsupportedJWEAlgorithm(alg, supportedJWEAlgorithms()));
         }
+        DecryptResponse decryptResponse;
+        if (keyRotationEnabled) {
+            System.out.println("KeyRotationEnabled!!!!!!!!");
+            var encryptedKeyDecryptRequestPrimary =
+                    DecryptRequest.builder()
+                            .ciphertextBlob(SdkBytes.fromByteArray(encryptedKey.decode()))
+                            .encryptionAlgorithm(RSAES_OAEP_SHA_256)
+                            .keyId("alias/" + CLIENT_JAR_KMS_ENCRYPTION_KEY_ALIAS_PRIMARY)
+                            .build();
 
-        DecryptRequest decryptRequest =
-                DecryptRequest.builder()
-                        .encryptionAlgorithm(EncryptionAlgorithmSpec.RSAES_OAEP_SHA_256)
-                        .ciphertextBlob(SdkBytes.fromByteArray(encryptedKey.decode()))
-                        .keyId(this.keyId)
-                        .build();
-        DecryptResponse decryptResponse = this.kmsClient.decrypt(decryptRequest);
+            var encryptedKeyDecryptRequestSecondary =
+                    DecryptRequest.builder()
+                            .ciphertextBlob(SdkBytes.fromByteArray(encryptedKey.decode()))
+                            .encryptionAlgorithm(RSAES_OAEP_SHA_256)
+                            .keyId("alias/" + CLIENT_JAR_KMS_ENCRYPTION_KEY_ALIAS_SECONDARY)
+                            .build();
+
+            // During a key rotation we might receive JWTs encrypted with either the old or new key.
+            try {
+                System.out.println("PRIMARY KEY BLOCK:::::");
+                decryptResponse = kmsClient.decrypt(encryptedKeyDecryptRequestPrimary);
+            } catch (IncorrectKeyException e) {
+                System.out.println("SECONDARY KEY BLOCK:::::");
+                decryptResponse = kmsClient.decrypt(encryptedKeyDecryptRequestSecondary);
+            } catch (Exception e) {
+                // We only expect to get IncorrectKeyExceptions, but if we get another error we
+                // should
+                // still try the secondary key
+                System.out.println("SECONDARY RETRY KEY BLOCK:::::");
+                LOGGER.warn(
+                        "Unexpected exception decrypting JWT key with primary key. Trying secondary key. %s"
+                                .formatted(e.getMessage()));
+                decryptResponse = kmsClient.decrypt(encryptedKeyDecryptRequestSecondary);
+            }
+
+        } else {
+
+            DecryptRequest decryptRequest =
+                    DecryptRequest.builder()
+                            .encryptionAlgorithm(EncryptionAlgorithmSpec.RSAES_OAEP_SHA_256)
+                            .ciphertextBlob(SdkBytes.fromByteArray(encryptedKey.decode()))
+                            .keyId(this.keyId)
+                            .build();
+            decryptResponse = this.kmsClient.decrypt(decryptRequest);
+        }
+
         SecretKey cek = new SecretKeySpec(decryptResponse.plaintext().asByteArray(), "AES");
-
         return ContentCryptoProvider.decrypt(
                 header, null, encryptedKey, iv, cipherText, authTag, cek, getJCAContext());
     }
