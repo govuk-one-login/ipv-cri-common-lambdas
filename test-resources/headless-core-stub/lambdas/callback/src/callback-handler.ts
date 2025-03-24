f591d50fb276febb0bb27870a1a7d416f2f7f594
Olakunle Jegede <olakunle.jegede@digital.cabinet-office.gov.uk>
2025-03-24 11:41:51 +0000

    OJ-2983: Implemented callback handler
    Co-authored-by: Suraj Kumar <suraj.kumar@digital.cabinet-office.gov.uk>

--- test-resources/headless-core-stub/lambdas/callback/src/callback-handler.ts
+++ test-resources/headless-core-stub/lambdas/callback/src/callback-handler.ts
@@ -1,12 +1,90 @@
-import type { LambdaInterface } from "@aws-lambda-powertools/commons/types";
+import { LambdaInterface } from "@aws-lambda-powertools/commons/types";
+import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
+import { Logger } from "@aws-lambda-powertools/logger";
+import { ConfigurationHelper } from "./services/configuration-helper";
+import { CallBackService } from "./services/callback-service";
+import { generatePrivateJwtParams } from "./services/private-key-jwt-helper";
 
+const sessionTableName = process.env.SESSION_TABLE || "session-common-cri-api";
+const configurationHelper = new ConfigurationHelper();
+const callbackService = new CallBackService();
+const logger = new Logger();
 export class CallbackLambdaHandler implements LambdaInterface {
-    async handler(): Promise<{ status: string; body: string }> {
-        return {
-            status: "200",
-            body: "Hello callback",
-        };
+    async handler(event: APIGatewayProxyEvent, _context: Context): Promise<APIGatewayProxyResult> {
+        try {
+            const authorizationCode = event.queryStringParameters?.authorizationCode as string;
+            logger.info({ message: "Received authorizationCode", authorizationCode });
+
+            const sessionItem = await callbackService.getSessionByAuthorizationCode(
+                sessionTableName,
+                authorizationCode,
+            );
+            logger.info({ message: "Fetching session item...", ...sessionItem });
+
+            const ssmParameters = await configurationHelper.getParameters(sessionItem.clientId);
+            const { privateSigningKey, ...filteredParams } = ssmParameters;
+
+            logger.info({ message: "Fetching SSM parameters", ...filteredParams });
+
+            const privateJwtKey = JSON.parse(privateSigningKey);
+            const audience = ssmParameters["audience"];
+
+            logger.info({ message: "Generating private JWT parameters..." });
+            const privateJwtParams = await generatePrivateJwtParams(
+                sessionItem.clientId,
+                authorizationCode,
+                sessionItem.redirectUri,
+                privateJwtKey,
+                audience,
+            );
+
+            const audienceApi = this.formatAudience(audience);
+            logger.info({ message: "Using Audience", audienceApi });
+
+            const tokenEndpoint = `${audienceApi}/token`;
+            logger.info({ message: "Calling token endpoint", tokenEndpoint, privateJwtParams });
+            const tokenResponse = await callbackService.getToken(tokenEndpoint, privateJwtParams);
+
+            if (!tokenResponse.ok) {
+                const tokenResponseBody = await tokenResponse.text();
+                const status = tokenResponse.status;
+                logger.error({ message: "Request to endpoint failed", tokenEndpoint, status, tokenResponseBody });
+                return { statusCode: tokenResponse.status, body: tokenResponseBody };
+            }
+
+            logger.info({ message: "Successfully called /token endpoint" });
+            const tokenBody = await tokenResponse.json();
+
+            const credentialEndpoint = `${audienceApi}/credential/issue`;
+            logger.info({ message: "Calling issue credential endpoint", credentialEndpoint });
+            const credential = await callbackService.callIssueCredential(credentialEndpoint, tokenBody.access_token);
+            const credentialResponseJwt = await credential.text();
+
+            if (!credential.ok) {
+                const endpoint = credentialEndpoint;
+                const status = credential.status;
+                const body = await credential.text();
+                logger.error({ message: "Request to endpoint failed", endpoint, status, body });
+            }
+            logger.info({ message: "Successfully called /credential/issue endpoint" });
+
+            return {
+                statusCode: credential.status,
+                headers: {
+                    "Content-Type": "application/jwt",
+                },
+                body: credentialResponseJwt,
+            };
+        } catch (error: unknown) {
+            const err = error as Error;
+            const value = err.message;
+            logger.error({ message: "Unknown error occurred", value });
+            return { statusCode: 500, body: err.message };
+        }
     }
+
+    private formatAudience = (audience: string) =>
+        audience.includes("review-") ? audience.replace("review-", "api.review-") : audience;
 }
 
 const handlerClass = new CallbackLambdaHandler();
--- test-resources/headless-core-stub/lambdas/callback/tests/callback-handler.test.ts
+++ test-resources/headless-core-stub/lambdas/callback/tests/callback-handler.test.ts
@@ -1,9 +1,158 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
 import { CallbackLambdaHandler } from "../src/callback-handler";
+import { CallBackService } from "../src/services/callback-service";
+import { ConfigurationHelper } from "../src/services/configuration-helper";
+import * as KeyJwtHelper from "../src/services/private-key-jwt-helper";
+import { SessionItem } from "../src/services/session-item";
+jest.mock("../src/services/callback-service");
+jest.mock("../src/services/configuration-helper");
 
 describe("callback-handler", () => {
-    it("Returns 200", async () => {
-        const callbackLambdaHandler = new CallbackLambdaHandler();
-        const result = await callbackLambdaHandler.handler();
-        expect(result).toEqual({ status: "200", body: "Hello callback" });
+    const sessionTableName = "session-common-cri-api";
+    const authorizationCode = "an-authorization-code";
+    const accessTokenValue = "access_token_value";
+    const clientId = "headless-core-stub";
+    const audience = "my-audience";
+    const redirectUri = "https://test-resources.headless-core-stub.redirect/callback";
+    const keyJwtValue =
+        "client_assertion_type=urn%3Aietf%3Aparams%3Aoauth%3Aclient-assertion-type%3Ajwt-bearer&code=an-authorization-code&grant_type=authorization_code&redirect_uri=https%3A%2F%2Ftest-resources.headless-core-stub.redirect%2Fcallback&client_assertion=fake.jwt.key";
+
+    let getTokenSpy: jest.SpyInstance;
+    let getParametersSpy: jest.SpyInstance;
+    let issueCredentialSpy: jest.SpyInstance;
+    let sessionByAuthorizationCodeSpy: jest.SpyInstance;
+    let lambdaHandler: (_event: APIGatewayProxyEvent, _context: Context) => Promise<APIGatewayProxyResult>;
+
+    beforeEach(() => {
+        const callbackHandler = new CallbackLambdaHandler();
+        lambdaHandler = callbackHandler.handler.bind(callbackHandler);
+    });
+    afterEach(() => jest.clearAllMocks());
+
+    it("returns 200 when entire flow is successful", async () => {
+        sessionByAuthorizationCodeSpy = jest
+            .spyOn(CallBackService.prototype, "getSessionByAuthorizationCode")
+            .mockResolvedValueOnce({ clientId, redirectUri } as SessionItem);
+
+        jest.spyOn(KeyJwtHelper, "generatePrivateJwtParams").mockResolvedValueOnce(keyJwtValue);
+
+        getParametersSpy = jest.spyOn(ConfigurationHelper.prototype, "getParameters").mockResolvedValueOnce({
+            redirectUri,
+            audience,
+            issuer: "https://issuer.example.com",
+            privateSigningKey: JSON.stringify({}),
+        });
+        getTokenSpy = jest.spyOn(CallBackService.prototype, "getToken").mockResolvedValueOnce({
+            ok: true,
+            status: 200,
+            json: async () => ({ access_token: accessTokenValue }),
+        } as Response);
+        issueCredentialSpy = jest.spyOn(CallBackService.prototype, "callIssueCredential").mockResolvedValueOnce({
+            ok: true,
+            status: 200,
+            text: async () => "vc.jwt.credential",
+        } as Response);
+
+        const response = await lambdaHandler(
+            {
+                queryStringParameters: { authorizationCode },
+            } as unknown as APIGatewayProxyEvent,
+            {} as Context,
+        );
+
+        expect(getParametersSpy).toHaveBeenCalledWith(clientId);
+        expect(getTokenSpy).toHaveBeenCalledWith("my-audience/token", keyJwtValue);
+        expect(issueCredentialSpy).toHaveBeenCalledWith("my-audience/credential/issue", "access_token_value");
+        expect(sessionByAuthorizationCodeSpy).toHaveBeenLastCalledWith("session-common-cri-api", authorizationCode);
+        expect(response).toEqual({
+            statusCode: 200,
+            headers: {
+                "Content-Type": "application/jwt",
+            },
+            body: "vc.jwt.credential",
+        });
+    });
+
+    it("returns 404 if session is not found", async () => {
+        sessionByAuthorizationCodeSpy = jest
+            .spyOn(CallBackService.prototype, "getSessionByAuthorizationCode")
+            .mockRejectedValueOnce(new Error("No session item found for provided authorizationCode"));
+
+        const event: Partial<APIGatewayProxyEvent> = { queryStringParameters: { authorizationCode } };
+        const response = await lambdaHandler(event as APIGatewayProxyEvent, {} as Context);
+
+        expect(sessionByAuthorizationCodeSpy).toHaveBeenCalledWith(sessionTableName, authorizationCode);
+        expect(response).toEqual({
+            statusCode: 500,
+            body: "No session item found for provided authorizationCode",
+        });
+    });
+
+    it("handles token endpoint failure gracefully", async () => {
+        const event: Partial<APIGatewayProxyEvent> = {
+            queryStringParameters: {
+                authorizationCode,
+            },
+        };
+        jest.spyOn(KeyJwtHelper, "generatePrivateJwtParams").mockResolvedValueOnce(keyJwtValue);
+
+        sessionByAuthorizationCodeSpy = jest
+            .spyOn(CallBackService.prototype, "getSessionByAuthorizationCode")
+            .mockResolvedValueOnce({
+                clientId,
+                redirectUri,
+            } as SessionItem);
+        getParametersSpy = jest.spyOn(ConfigurationHelper.prototype, "getParameters").mockResolvedValueOnce({
+            redirectUri,
+            audience,
+            issuer: "https://issuer.example.com",
+            privateSigningKey: JSON.stringify({}),
+        });
+        getTokenSpy = jest
+            .spyOn(CallBackService.prototype, "getToken")
+            .mockResolvedValueOnce({ ok: false, status: 500, text: async () => "mock-token-error" } as Response);
+
+        const response = await lambdaHandler(event as APIGatewayProxyEvent, {} as Context);
+
+        expect(sessionByAuthorizationCodeSpy).toHaveBeenCalledWith(sessionTableName, authorizationCode);
+        expect(getParametersSpy).toHaveBeenCalledWith(clientId);
+        expect(response.statusCode).toBe(500);
+        expect(response.body).toBe("mock-token-error");
+    });
+
+    it("handles credential endpoint failure gracefully", async () => {
+        const event: Partial<APIGatewayProxyEvent> = {
+            queryStringParameters: {
+                authorizationCode,
+            },
+        };
+
+        jest.spyOn(KeyJwtHelper, "generatePrivateJwtParams").mockResolvedValueOnce(keyJwtValue);
+        sessionByAuthorizationCodeSpy = jest
+            .spyOn(CallBackService.prototype, "getSessionByAuthorizationCode")
+            .mockResolvedValueOnce({ clientId, redirectUri } as SessionItem);
+        getParametersSpy = jest.spyOn(ConfigurationHelper.prototype, "getParameters").mockResolvedValueOnce({
+            redirectUri,
+            audience,
+            issuer: "https://issuer.example.com",
+            privateSigningKey: JSON.stringify({}),
+        });
+        getTokenSpy = jest.spyOn(CallBackService.prototype, "getToken").mockResolvedValueOnce({
+            ok: true,
+            json: async () => ({ access_token: accessTokenValue }),
+        } as Response);
+        issueCredentialSpy = jest.spyOn(CallBackService.prototype, "callIssueCredential").mockResolvedValueOnce({
+            ok: false,
+            status: 500,
+            text: async () => "mock-credential-error",
+        } as Response);
+
+        const response = await lambdaHandler(event as APIGatewayProxyEvent, {} as Context);
+
+        expect(sessionByAuthorizationCodeSpy).toHaveBeenCalledWith(sessionTableName, authorizationCode);
+        expect(getParametersSpy).toHaveBeenCalledWith(clientId);
+        expect(issueCredentialSpy).toHaveBeenCalledWith(expect.any(String), accessTokenValue);
+        expect(response.statusCode).toBe(500);
+        expect(response.body).toBe("mock-credential-error");
     });
 });
