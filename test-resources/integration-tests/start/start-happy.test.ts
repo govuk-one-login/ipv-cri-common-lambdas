2fc9f81c5d8045e279db1f7bb9843c02658a7eff
Caitlin Cooling <caitlin.cooling@digital.cabinet-office.gov.uk>
2025-03-28 14:12:47 +0000

    OJ-3122: Sending signed request to start endpoint

--- test-resources/integration-tests/start/start-happy.test.ts
+++ test-resources/integration-tests/start/start-happy.test.ts
@@ -1,13 +1,35 @@
+import { fromNodeProviderChain } from "@aws-sdk/credential-providers";
+import { createSignedFetcher } from "aws-sigv4-fetch";
 import { stackOutputs } from "../helpers/cloudformation";
 
+const customCredentialsProvider = fromNodeProviderChain({
+    timeout: 1000,
+    maxRetries: 0,
+});
+
+const signedFetch = createSignedFetcher({
+    region: "eu-west-2",
+    service: "execute-api",
+    credentials: customCredentialsProvider,
+});
+
 describe("core stub start endpoint", () => {
-    let output;
+    let TestHarnessExecuteUrl;
 
     beforeAll(async () => {
-        output = await stackOutputs(process.env.STACK_NAME);
-        console.log("output", output);
+        ({ TestHarnessExecuteUrl } = await stackOutputs(process.env.STACK_NAME));
     });
-    it("works", () => {
-        expect(true).toBe(true);
+    it("returns 200 for a valid request", async () => {
+        const response = await signedFetch(`${TestHarnessExecuteUrl}start`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ aud: "https://review-a.dev.account.gov.uk" }),
+        });
+
+        console.log("TestHarnessExecuteUrl", TestHarnessExecuteUrl);
+        // console.log("response", response);
+        expect(response.status).toBe(200);
     });
 });
