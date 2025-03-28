6171551b32e890ba017e3a780e64b87fc2c81673
Caitlin Cooling <caitlin.cooling@digital.cabinet-office.gov.uk>
2025-03-28 13:20:07 +0000

    OJ-3122: Getting stack outputs

--- test-resources/integration-tests/start/start-happy.test.ts
+++ test-resources/integration-tests/start/start-happy.test.ts
@@ -1,4 +1,12 @@
+import { stackOutputs } from "../helpers/cloudformation";
+
 describe("core stub start endpoint", () => {
+    let output;
+
+    beforeAll(async () => {
+        output = await stackOutputs(process.env.STACK_NAME);
+        console.log("output", output);
+    });
     it("works", () => {
         expect(true).toBe(true);
     });
