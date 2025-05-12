ea8275f4115009270a7360d0ee677b7c0252f0f3
Caitlin Cooling <caitlin.cooling@digital.cabinet-office.gov.uk>
2025-05-12 10:56:48 +0100

    OJ-3075: Update imports

--- session/src/test/java/uk/gov/di/ipv/cri/common/api/handler/SessionHandlerTest.java
+++ session/src/test/java/uk/gov/di/ipv/cri/common/api/handler/SessionHandlerTest.java
@@ -40,7 +40,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.isNull;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static uk.gov.di.ipv.cri.common.api.handler.SessionHandler.REDIRECT_URI;
 import static uk.gov.di.ipv.cri.common.api.handler.SessionHandler.STATE;
 
