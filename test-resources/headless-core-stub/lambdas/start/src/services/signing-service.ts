c1450a354aec3741204f7579aae2c5decafdd323
Olakunle Jegede <olakunle.jegede@digital.cabinet-office.gov.uk>
2025-05-16 15:37:27 +0100

    OJ-2969: Implemented call ./well-known endpoint of CRI
    This PR useUse KEY_ROTATION_FEATURE_FLAG_ENABLED environment variable
    
    Instead of getting the key from KMS of the CRI,
    when the feature flag is enabled KEY_ROTATION_FEATURE_FLAG_ENABLED
    
    The headless core stub now calls the ./well-known endpoint of the CRI to retrieve
    the public encryption key used to encrypt the JAR.

--- test-resources/headless-core-stub/lambdas/start/src/services/signing-service.ts
+++ test-resources/headless-core-stub/lambdas/start/src/services/signing-service.ts
@@ -1,16 +1,63 @@
 import { GetPublicKeyCommand, KMSClient } from "@aws-sdk/client-kms";
-import { CompactEncrypt, importSPKI, KeyLike } from "jose";
+import { CompactEncrypt, importJWK, importSPKI, JSONWebKeySet, JWK, KeyLike } from "jose";
 import { HeadlessCoreStubError } from "../../../../utils/src/errors/headless-core-stub-error";
+import { formatAudience } from "../../../../utils/src/audience-formatter";
+import { Logger } from "@aws-lambda-powertools/logger";
 
 const kmsClient = new KMSClient({ region: "eu-west-2" });
+export const logger = new Logger();
 
 let cachedPublicKey: KeyLike | undefined;
 
-export const getPublicEncryptionKey = async () => {
+export const getPublicEncryptionKey = async (audience: string) => {
     if (cachedPublicKey) {
         return cachedPublicKey;
     }
 
+    if (process.env.KEY_ROTATION_FEATURE_FLAG_ENABLED === "true") {
+        return await getPublicEncryptionKeyJwksUri(audience);
+    }
+
+    if (!cachedPublicKey) {
+        logger.info({ message: "using KMS to retrieve encryption key" });
+        await getPublicEncryptionKeyFromKms();
+    }
+
+    return cachedPublicKey;
+};
+
+export const encryptSignedJwt = (signedJwt: string, publicEncryptionKey: KeyLike) => {
+    return new CompactEncrypt(new TextEncoder().encode(signedJwt))
+        .setProtectedHeader({ alg: "RSA-OAEP-256", enc: "A256GCM" })
+        .encrypt(publicEncryptionKey);
+};
+
+async function getPublicEncryptionKeyJwksUri(audience: string) {
+    const audienceApi = formatAudience(audience);
+    const criEncryptionJwksEndpoint = new URL("/.well-known/jwks.json", audienceApi).href;
+
+    logger.info({ message: "Attempting to use CRI hosted public encryption jwks endpoint", criEncryptionJwksEndpoint });
+
+    const data = await fetch(criEncryptionJwksEndpoint, {
+        method: "GET",
+        headers: { Accept: "application/json" },
+    });
+
+    if (!data.ok) {
+        throw new Error(`Failed to fetch JWKS: ${data.status} ${data.statusText}`);
+    }
+
+    const jwks = (await data.json()) as JSONWebKeySet;
+
+    if (!jwks.keys || !Array.isArray(jwks.keys) || jwks.keys.length === 0) {
+        throw new Error(`Invalid or empty JWKS response from ${criEncryptionJwksEndpoint}`);
+    }
+    logger.info({ message: "Successfully retrieved public encryption jwks endpoint", ...jwks });
+
+    cachedPublicKey = (await importJWK(jwks.keys.find((k) => k.use === "enc") as JWK, "RSA-OAEP-256")) as KeyLike;
+}
+
+async function getPublicEncryptionKeyFromKms() {
     const decryptionKeyId = process.env.DECRYPTION_KEY_ID;
     if (!decryptionKeyId) {
         throw new HeadlessCoreStubError("Decryption key ID not present", 500);
@@ -28,11 +75,4 @@ export const getPublicEncryptionKey = async () => {
     const publicKeyPem = `${header}\n${value}\n${footer}`;
 
     cachedPublicKey = await importSPKI(publicKeyPem, "RS256");
-    return cachedPublicKey;
-};
-
-export const encryptSignedJwt = (signedJwt: string, publicEncryptionKey: KeyLike) => {
-    return new CompactEncrypt(new TextEncoder().encode(signedJwt))
-        .setProtectedHeader({ alg: "RSA-OAEP-256", enc: "A256GCM" })
-        .encrypt(publicEncryptionKey);
-};
+}
--- test-resources/headless-core-stub/lambdas/start/tests/services/signing-service.test.ts
+++ test-resources/headless-core-stub/lambdas/start/tests/services/signing-service.test.ts
@@ -5,9 +5,11 @@ import { generateKeyPairSync } from "crypto";
 import { HeadlessCoreStubError } from "../../../../utils/src/errors/headless-core-stub-error";
 import { encryptSignedJwt, getPublicEncryptionKey } from "../../src/services/signing-service";
 import { TestData } from "../../../../utils/tests/test-data";
+import { KeyLike } from "jose";
 
-describe("crypto-service", () => {
-    describe("getPublicEncryptionKey", () => {
+describe("signing-service", () => {
+    const audience = "https://test-audience.com";
+    describe("getPublicEncryptionKey - kms", () => {
         const mockKMSClient = mockClient(KMSClient);
 
         afterEach(() => {
@@ -16,14 +18,14 @@ describe("crypto-service", () => {
         });
 
         it("throws error with 500 if decryption key env variable not set", async () => {
-            await expect(getPublicEncryptionKey()).rejects.toThrow(
+            await expect(getPublicEncryptionKey(audience)).rejects.toThrow(
                 new HeadlessCoreStubError("Decryption key ID not present", 500),
             );
         });
 
         it("throws error with 500 if kms key not retrieved", async () => {
             process.env.DECRYPTION_KEY_ID = "abc123";
-            await expect(getPublicEncryptionKey()).rejects.toThrow(
+            await expect(getPublicEncryptionKey(audience)).rejects.toThrow(
                 new HeadlessCoreStubError("Unable to retrieve public encryption key", 500),
             );
         });
@@ -47,8 +49,65 @@ describe("crypto-service", () => {
             const mockKMSClient = mockClient(KMSClient);
             mockKMSClient.on(GetPublicKeyCommand, { KeyId: "abc123" }).resolvesOnce({ PublicKey: keyBuffer });
 
-            const result = await getPublicEncryptionKey();
-            expect(result.type).toEqual("public");
+            const result = await getPublicEncryptionKey(audience);
+            expect(result?.type).toEqual("public");
+        });
+    });
+
+    describe("getPublicEncryptionKey - jwks uri", () => {
+        let originalFetch: typeof global.fetch;
+
+        beforeEach(() => {
+            originalFetch = global.fetch;
+        });
+
+        afterEach(() => {
+            global.fetch = originalFetch;
+            clearCaches();
+            delete process.env.DECRYPTION_KEY_ID;
+        });
+
+        it("retrieves public encryption key from JWKS endpoint", async () => {
+            const mockJwks = {
+                keys: [{ kty: "RSA", e: "AQAB", use: "enc", alg: "RS256", n: "dummy-n", kid: "dummy-kid" }],
+            };
+            process.env.KEY_ROTATION_FEATURE_FLAG_ENABLED == "true";
+
+            global.fetch = jest.fn().mockResolvedValueOnce({
+                ok: true,
+                json: () => Promise.resolve(mockJwks),
+            });
+
+            const result = await getPublicEncryptionKey("https://test-audience.co.uk");
+
+            expect(result).toBeDefined();
+            expect(result?.type).toBe("public");
+        });
+
+        it("falls back to KMS if JWKS fetch fails", async () => {
+            process.env.KEY_ROTATION_FEATURE_FLAG_ENABLED == "false";
+
+            const { publicKey } = generateKeyPairSync("rsa", {
+                modulusLength: 2048,
+                publicKeyEncoding: {
+                    type: "spki",
+                    format: "der",
+                },
+                privateKeyEncoding: {
+                    type: "pkcs8",
+                    format: "der",
+                },
+            });
+
+            const keyBuffer = Buffer.from(publicKey);
+            process.env.DECRYPTION_KEY_ID = "abc123";
+
+            const mockKMSClient = mockClient(KMSClient);
+            mockKMSClient.on(GetPublicKeyCommand, { KeyId: "abc123" }).resolvesOnce({ PublicKey: keyBuffer });
+
+            const result = await getPublicEncryptionKey(audience);
+            expect(result).toBeDefined();
+            expect(result?.type).toBe("public");
         });
     });
 
@@ -72,9 +131,9 @@ describe("crypto-service", () => {
             const mockKMSClient = mockClient(KMSClient);
             mockKMSClient.on(GetPublicKeyCommand, { KeyId: "abc123" }).resolvesOnce({ PublicKey: keyBuffer });
 
-            const publicEncryptionKey = await getPublicEncryptionKey();
+            const publicEncryptionKey = await getPublicEncryptionKey(audience);
 
-            const result = await encryptSignedJwt(TestData.jwt, publicEncryptionKey);
+            const result = await encryptSignedJwt(TestData.jwt, publicEncryptionKey as KeyLike);
             expect(result).toMatch(
                 /^eyJ[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+$/g,
             );
