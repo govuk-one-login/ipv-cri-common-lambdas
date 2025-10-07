d4079f0f2fdfa42e57788fff317da612ef3c2376
MarieseMikely <mariese.mikely@digital.cabinet-office.gov.uk>
2025-10-07 10:30:46 +0100

    OJ-2550: fix - remove extensions.evidence.context from the send audit implementation in the session handler

--- lambdas/src/handlers/session-handler.ts
+++ lambdas/src/handlers/session-handler.ts
@@ -126,18 +126,11 @@ export class SessionLambda implements LambdaInterface {
                 },
             }),
             ...(evidenceRequested && {
-                extensions: {
-                    evidence: [
-                        {
-                            context: "identity_check",
-                        },
-                    ],
-                    ...(evidenceRequested.verificationScore && {
-                        evidence_requested: {
-                            verificationScore: evidenceRequested.verificationScore,
-                        },
-                    }),
-                },
+                ...(evidenceRequested.verificationScore && {
+                    evidence_requested: {
+                        verificationScore: evidenceRequested.verificationScore,
+                    },
+                }),
             }),
         });
     }
