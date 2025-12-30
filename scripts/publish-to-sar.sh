4043341811beb4a7c7ac6d161208255904ce097d
Barnaby Collins <23247109+barnabycollins@users.noreply.github.com>
2025-12-30 10:42:06 +0000

    OJ-3523: Add stack outputs for consumption by parent

--- scripts/publish-to-sar.sh
+++ scripts/publish-to-sar.sh
@@ -4,10 +4,13 @@ set -e
 
 export AWS_PROFILE="common-dev-admin"
 
-aws sso login
+# log into sso if no valid session currently exists
+aws sts get-caller-identity || aws sso login
 
-SRC_TEMPLATE="infrastructure/lambda/template.yaml"
-PKG_TEMPLATE="packaged.yaml"
+SRC_TEMPLATE="infrastructure/lambda/template.yaml" # used for the build steps
+BUILT_TEMPLATE=".aws-sam/build/template.yaml" # output from the build step, used for the package step
+PKG_TEMPLATE="packaged.yaml" # output from the package step, used for the publish step
+
 BUCKET_NAME="barnaby-serverless-app-repo-test-bucket"
 REGION="eu-west-2"
 APPLICATION_NAME="bc-test-commonlambdas-2025-12"
@@ -22,7 +25,7 @@ sam build --template $SRC_TEMPLATE --cached --parallel
 
 echo "built"
 
-sam package --template $SRC_TEMPLATE --output-template-file $PKG_TEMPLATE --s3-bucket $BUCKET_NAME
+sam package --template $BUILT_TEMPLATE --output-template-file $PKG_TEMPLATE --s3-bucket $BUCKET_NAME
 
 echo "packaged"
 
