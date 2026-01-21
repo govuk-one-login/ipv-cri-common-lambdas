3c901b1cbc9a13dbc3cf9430b892d4386520406b
Barnaby Collins <23247109+barnabycollins@users.noreply.github.com>
2026-01-21 16:58:17 +0000

    OJ-3558: test-resources stack cleanup

--- test-resources/headless-core-stub/utils/src/base64/index.ts
+++
@@ -1,5 +0,0 @@
-import { Buffer } from "buffer";
-
-export const base64Encode = (stringToEncode: string) => Buffer.from(stringToEncode, "binary").toString("base64");
-
-export const base64Decode = (base64String: string) => Buffer.from(base64String, "base64").toString("binary");
