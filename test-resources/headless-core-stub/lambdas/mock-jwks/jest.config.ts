77bafb9df90cb9c3c03f465c1220c41452689b2f
Barnaby Collins <23247109+barnabycollins@users.noreply.github.com>
2026-01-21 16:58:17 +0000

    OJ-3558: test-resources stack cleanup

--- test-resources/headless-core-stub/lambdas/mock-jwks/jest.config.ts
+++
@@ -1,7 +0,0 @@
-import type { Config } from "jest";
-import baseConfig from "../../../jest.config.base";
-
-export default {
-    ...baseConfig,
-    displayName: "headless-core-stub/lambdas/mock-jwks",
-} satisfies Config;
--- test-resources/headless-core-stub/utils/src/hashing/index.ts
+++
@@ -1,7 +0,0 @@
-import { createHash } from "crypto";
-
-export const getHashedKid = (keyId: string) => {
-    const kidBytes = Buffer.from(keyId, "utf8");
-    const hash = createHash("sha256").update(kidBytes).digest();
-    return Buffer.from(hash).toString("hex");
-};
