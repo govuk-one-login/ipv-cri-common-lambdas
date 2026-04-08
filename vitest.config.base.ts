d33a13308420cbcd92bbf72a6626034792d6be80
MarieseMikely <mariese.mikely@digital.cabinet-office.gov.uk>
2026-04-08 11:23:09 +0100

    OJ-3610: feat - update the root directory to include the updated vitest config and ran npm audit fix
    Signed-off-by: MarieseMikely <mariese.mikely@digital.cabinet-office.gov.uk>

---
+++ vitest.config.base.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    clearMocks: true,
+    include: ["tests/**/*.test.ts"],
+    coverage: {
+      provider: "v8",
+      include: ["src/**/*"],
+      thresholds: {
+        statements: 100,
+        branches: 100,
+        functions: 100,
+        lines: 100,
+      },
+    },
+  },
+});
---
+++ vitests.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    projects: ["lambdas/vitest.config.ts", "test-resources/vitest.config.ts" ],
+  },
+});
